1330a554d6467a8367d14261c77bbc6ca9b41e55
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-07-01 13:08:32 +0900

    Vector2クラスを生成

--- Vector2.h
+++ Vector2.h
@@ -20,5 +20,21 @@ public:
 	float cross(const Vector2& v);	//外積を返す
 
 	//単項演算子オーバーロード
+	Vector2 operator+() const;
+	Vector2 operator-() const;
+
+	//代入演算子オーバーロード
+	Vector2& operator+=(const Vector2& v);
+	Vector2& operator-=(const Vector2& v);
+	Vector2& operator*=(float s);
+	Vector2& operator/=(float s);
 };
 
+
+//2項演算子オーバーロード
+//いろんな引数(引数の型と順序)のパターンに対応するため、以下のように準備している
+const Vector2 operator+(const Vector2& v1, const Vector2& v2); 
+const Vector2 operator-(const Vector2& v1, const Vector2& v2); 
+const Vector2 operator*(const Vector2& v,float s);
+const Vector2 operator*(float s,const Vector2& v);
+const Vector2 operator/(const Vector2& v, float s);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "DxLib.h"
+#include "Vector2.h"
+
+int DrawCircle(Vector2 vec, int r, unsigned int color);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
@@ -12,18 +15,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	if (DxLib_Init() == -1) return -1;
 	SetDrawScreen(DX_SCREEN_BACK);
 
+	//ゲームループで使う変数の宣言
+	Vector2 position(10.0f, 100.0f);
+	Vector2 velocity(+1.0f, +0.5f);
+
 	//ゲームループ
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		ClearDrawScreen();
 
 		//更新
+		position += velocity;
 
 		//描画
-		DrawCircle(WindowWidth / 2, WindowHeight / 2, 16, GetColor(128, 255, 128));
+		DrawCircle(position,16,GetColor(128,155,128));
 
 		ScreenFlip();
 	}
 	DxLib_End();
 	return 0;
+}
+
+int DrawCircle(Vector2 vec, int r, unsigned int color)
+{
+	return DrawCircle(static_cast<int>(vec.getX()), static_cast<int>(vec.getY()), r, color);
 }
